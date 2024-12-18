aa2fcfb5be02046b6238f994eec1080ba7a5b67c
whiterven <163173007+whiterven@users.noreply.github.com>
2024-12-18 15:11:47 -0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,16 +8,22 @@ if __name__ == "__main__":
     port = int(os.getenv("PORT", 10000))
     webhook_url = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/{TELEGRAM_BOT_TOKEN}"
     
-    try:
-        bot.remove_webhook()
-        bot.set_webhook(
-            url=webhook_url,
-            max_connections=40,
-            allowed_updates=["message", "callback_query"],
-            drop_pending_updates=True
-        )
-        logging.info(f"Webhook set to {webhook_url}")
-    except Exception as e:
-        logging.error(f"Failed to set webhook: {e}")
+    if os.getenv('RENDER_EXTERNAL_HOSTNAME'):
+        try:
+            logging.info("Setting up webhook...")
+            bot.remove_webhook()
+            bot.set_webhook(
+                url=webhook_url,
+                max_connections=40,
+                allowed_updates=["message", "callback_query"],
+                drop_pending_updates=True
+            )
+            logging.info(f"Webhook set to {webhook_url}")
+            
+        except Exception as e:
+            logging.error(f"Failed to set webhook: {e}")
+    else:
+          logging.info("Running locally without webhook")
+          # bot.polling(non_stop=True) # Use polling for local testing
     
     application.run(host='0.0.0.0', port=port)
