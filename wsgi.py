f94834cbfa4fb333f75fc3453264afcb25cb9444
whiterven <163173007+whiterven@users.noreply.github.com>
2024-12-18 13:43:21 -0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,15 @@
-from app import web_app as application
+from paste import application
+
+# Import the Flask app
+web_app = application.web_app
 
 if __name__ == "__main__":
-    application.run()
+    web_app.run()
+
+
+
+
+# from app import web_app as application
+
+# if __name__ == "__main__":
+#     application.run()
